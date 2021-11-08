1c842e156823123b102581ee8921dc55f0a44181
nahidsaikat <nahidsaikatft40@gmail.com>
2021-11-09 00:25:51 +0600

    Add everything for employee

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from employees.models import Employee
+from employees.serializers import EmployeeSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing employees.
+    """
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+    # permission_classes = [IsAuthenticated]
